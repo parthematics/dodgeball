bba39c3bc3d9737b2e4db9ee03b70e77b06a2abd
Parth Chopra <parthematics@gmail.com>
2018-07-18 00:23:24 -0700

    added game loop and renderer

--- src/project/main/Frame.java
+++ src/project/main/Frame.java
@@ -4,11 +4,11 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class Window extends Canvas {
+public class Frame extends Canvas {
 
     private static final long serialVersionUID = -2413443434825455921L;
 
-    public Window(int width, int height, String name, Game g) {
+    public Frame(int width, int height, String name, Game game) {
         JFrame frame = new JFrame(name);
 
         frame.setPreferredSize(new Dimension(width, height));
@@ -18,8 +18,8 @@ public class Window extends Canvas {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
-        frame.add(g);
+        frame.add(game);
         frame.setVisible(true);
-        g.start();
+        game.start();
     }
 }
--- src/project/main/Game.java
+++ src/project/main/Game.java
@@ -1,19 +1,83 @@
+/**
+ * @author Parth Chopra 2018
+ */
+
 package project.main;
 
-import java.awt.Canvas;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
+    public final int WIDTH = 640, HEIGHT = WIDTH * 3 / 4;
+    private Thread thread;
+    private boolean isRunning = false;
+
+    public Game() {
+        new Frame(WIDTH, HEIGHT, "dodgeball", this);
+    }
+
     public synchronized void start() {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            isRunning = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** @source As cited by Zachary Berenger. */
+    public void run() {
+        long prevTime = System.nanoTime();
+        double numTicks = 60.0, ns = 1000000000 / numTicks, delta = 0;
+        long timer = System.currentTimeMillis();
+        int numFrames = 0;
+        while (isRunning) {
+            long currTime = System.nanoTime();
+            delta += (currTime - prevTime) / ns;
+            prevTime = currTime;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (isRunning) {
+                render();
+            }
+            numFrames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + numFrames);
+                numFrames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
 
     }
 
-    public void run() {
+    private void render() {
+        BufferStrategy strat = this.getBufferStrategy();
+        if (strat == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = strat.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
 
+        g.dispose();
+        strat.show();
     }
 
     public static void main(String[] args) {
-
+        new Game();
     }
-
 }
